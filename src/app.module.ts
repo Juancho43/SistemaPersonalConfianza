a402361f8f777a7f0c0d5fa12161bc0543783c2e
juan <bravo43juancho@gmail.com>
2025-11-24 09:23:49 -0300

    Feat: remove unused controllers and services from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import config from './mikro-orm.config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { GoalModule } from './goal/goal.module';
 import { ProfileModule } from './profile/profile.module';
-import { GetGoalsController } from './profile/controllers/get-goals/get-goals.controller';
-import { UpdateProfileController } from './profile/controllers/update-profile/update-profile.controller';
 
 @Module({
   imports: [MikroOrmModule.forRoot(config), GoalModule, ProfileModule],
-  controllers: [AppController, UpdateProfileController],
-  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
